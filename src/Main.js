2dd4f9612fa8cee59847838d2809a01d583edcef
ANISH JOY <anishjoyz2002@gmail.com>
2024-07-19 11:11:30 +0530

    updatestyles

--- src/Main.js
+++ src/Main.js
@@ -1,42 +1,22 @@
 import React from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles.css";
+import img1 from "./fortnite.png";
 
 const Main = () => {
   return (
     <>
       <div className="header py-5">
-        <h1
-          className="mt-5 mb-5"
-          style={{
-            fontFamily: "Roboto, sans-serif",
-            fontSize: "43px",
-            fontWeight: "200",
-            lineHeight: "48px",
-            letterSpacing: "0.96px",
-            color: "#ffffff",
-          }}
-        >
+        <h1 className="mt-5 mb-5 ">
           Epic Games : An American Video Game And Software Developer <br /> And
           Publisher Based In Cary, North Carolina.
         </h1>
-        <div className="image-container">
-          <img
-            src="https://wallpapers.com/images/featured/fortnite-pictures-b5kfcchwazwiz3cs.jpg"
-            alt="Fortnite"
-            className="img-fluid my-4 "
-          />
-        </div>
-        <p
-          style={{
-            fontFamily: "Poppins, sans-serif",
-            fontWeight: 400,
-            fontSize: "18px",
-            lineHeight: "27px",
-            textAlign: "center",
-            color: "#ffffff",
-          }}
-        >
+        <Container>
+          <img src={img1} alt="Fortnite" className="img-fluid my-4 w-100" />
+        </Container>
+
+        <p className="subheading">
           Create, play, and battle with friends for free in Fortnite.Be the last
           player standing in Battle Royale and Zero Build experience a concert
           or live event,or <br /> discover a million creator made games ,
@@ -44,25 +24,7 @@ const Main = () => {
           island has <br /> an individual age rating so you can find the one
           that's right for your friends.Find it all in fortnite_Drop in.
         </p>
-        <Button
-          className="mt-4"
-          style={{
-            width: "416px",
-            height: "60px",
-            borderColor: "unset",
-            backgroundColor: "white",
-            fontFamily: "Poppins, sans-serif",
-            fontSize: "19px",
-            fontWeight: "400",
-            lineHeight: "28.5px",
-            letterSpacing: "0.96px",
-            color: "black",
-            textAlign: "center",
-            borderRadius: "unset",
-          }}
-        >
-          Visit Website
-        </Button>
+        <Button className="visitBTN mt-4">Visit Website</Button>
       </div>
 
       <Container>
@@ -146,17 +108,7 @@ const Main = () => {
           >
             Our Contribution
           </h2>
-          <p
-            className="pt-3"
-            style={{
-              fontFamily: "Poppins, sans-serif",
-              fontWeight: 400,
-              fontSize: "18px",
-              lineHeight: "27px",
-              textAlign: "center",
-              color: "#ffffff",
-            }}
-          >
+          <p className="subhead2 pt-3">
             Our core offering extends beyond mere profit generation; we
             emphasize the growth and active involvement of our user <br />{" "}
             community. Collaborating with us represents an investment, rather
@@ -166,78 +118,21 @@ const Main = () => {
           </p>
           <Row className="mt-5 pt-5 pb-5">
             <Col md={4}>
-              <h3
-                style={{
-                  fontWeight: 200,
-                  fontSize: "80px",
-                  lineHeight: "17.57px",
-                  textTransform: "capitalize",
-                }}
-              >
-                5M
-              </h3>
-              <p
-                className="pt-5"
-                style={{
-                  fontFamily: "Roboto, sans-serif",
-                  fontWeight: 300,
-                  fontSize: "25px",
-                  lineHeight: "29.3px",
-                  textAlign: "center",
-                  textTransform: "capitalize",
-                }}
-              >
+              <h3>5M</h3>
+              <p className="pt-5 subhead3">
                 Daily User
                 <br /> Engagements
               </p>
             </Col>
             <Col md={4}>
-              <h3
-                style={{
-                  fontWeight: 200,
-                  fontSize: "80px",
-                  lineHeight: "17.57px",
-                  textTransform: "capitalize",
-                }}
-              >
-                $500K
-              </h3>
-              <p
-                className="pt-5"
-                style={{
-                  fontFamily: "Roboto, sans-serif",
-                  fontWeight: 300,
-                  fontSize: "25px",
-                  lineHeight: "29.3px",
-                  textAlign: "center",
-                  textTransform: "capitalize",
-                }}
-              >
+              <h3>$500K</h3>
+              <p className="pt-5 subhead3">
                 Revenue Surge for our an <br /> Platform
               </p>
             </Col>
             <Col md={4}>
-              <h3
-                style={{
-                  fontWeight: 200,
-                  fontSize: "80px",
-                  lineHeight: "17.57px",
-                  textTransform: "capitalize",
-                }}
-              >
-                10X
-              </h3>
-              <p
-                className="pt-5"
-                style={{
-                  fontFamily: "Roboto, sans-serif",
-                  fontWeight: 300,
-                  fontSize: "25px",
-                  lineHeight: "29.3px",
-                  textAlign: "center",
-                  textTransform: "capitalize",
-                }}
-              >
+              <h3>10X</h3>
+              <p className="pt-5 subhead3">
                 ROAS on all our <br /> marketing campaigns
               </p>
             </Col>
@@ -245,76 +140,29 @@ const Main = () => {
         </div>
 
         <div className="my-5">
-          <h4
-            className="mt-5 pt-5 pb-5"
-            style={{
-              fontFamily: "Roboto, sans-serif",
-              fontWeight: 300,
-              fontSize: "45px",
-              lineHeight: "48px",
-              letterSpacing: "4px",
-            }}
-          >
+          <h4 className="mt-5 pt-5 pb-5">
             Interested In Delving Deeper Into The Project?
           </h4>
-          <p
-            className="pb-5"
-            style={{
-              fontFamily: "Roboto, sans-serif",
-              fontWeight: 300,
-              fontSize: "28px",
-              lineHeight: "32.81px",
-            }}
-          >
+          <p className="pb-5 subhead4">
             If you'd like to explore further details about our initiatives or
             any of our <br />
             affiliated brands, don't hesitate to connect. You can reach out to
             us via <br />
-            email at <span style={{ fontWeight: 400 }}>hello@abc.com</span> or
-            give us a call at
-            <span style={{ fontWeight: 400 }}> +91 420 20802730.</span>
+            email at{" "}
+            <b style={{ fontWeight: 800, fontSize: "22px" }}>
+              hello@abc.com
+            </b>{" "}
+            or give us a call at
+            <span style={{ fontWeight: 800, fontSize: "22px" }}>
+              {" "}
+              +91 420 20802730.
+            </span>
           </p>
-          <Button
-            style={{
-              backgroundColor: "black",
-              color: "white",
-              width: "23%",
-              marginRight: "5%",
-              fontFamily: "Poppins, sans-serif",
-              fontWeight: 500,
-              fontSize: "18px",
-              lineHeight: "27px",
-              border: "1px solid white",
-              borderRadius: "unset",
-              padding: "10px 20px",
-              textAlign: "center",
-              display: "inline-block",
-            }}
-          >
-            Sign up on Skype
-          </Button>
-          <Button
-            style={{
-              backgroundColor: "white",
-
-              width: "23%",
-              color: "black",
-              fontFamily: "Poppins, sans-serif",
-              fontWeight: 500,
-              fontSize: "18px",
-              lineHeight: "27px",
-              border: "1px solid white",
-              padding: "10px 20px",
-              textAlign: "center",
-              display: "inline-block",
-              borderRadius: "unset",
-            }}
-          >
-            Contact Us
-          </Button>
+          <Button className="SIgnupBtn ">Sign up on Skype</Button>
+          <Button className="ContactBTN">Contact Us</Button>
         </div>
 
-        <footer className="text-center mt-5 pb-5">
+        <footer className="text-center pt-5 pb-5">
           &copy; 2019-2024 ABC Technology Solutions — UAE. All Rights Reserved.
         </footer>
       </Container>
